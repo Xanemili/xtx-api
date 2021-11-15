ddb2688a2e247d5f8e6e34eee29f683ab80ad65c
Xander <emili.xander@gmail.com>
2021-11-15 13:33:36 -0900

    cors errors go away

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const { retrieveEODAssetPrices, updatePortfolioValuesDB } = require('./database_
 const app = express();
 
 app.use(cors({
-  origin: 'https://xtx-test-exchange.netlify.app/'
+  origin: 'https://xtx-test-exchange.netlify.app'
 }));
 app.use(helmet({ hsts: false }));
 app.use(logger('dev'));
